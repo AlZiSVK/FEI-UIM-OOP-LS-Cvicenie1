74cb8759da2a01e876dd6af90f2d2afe86cb8636
AlZiSVK <zichacekjunior@gmail.com>
2022-02-17 00:36:14 +0100

    typy premennych

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,4 +1,16 @@
 package sk.stuba.fei.uim.oop;
 
 public class App {
+    public static void main(String[] args) {
+        int celeCislo = 5;
+        long celeVacsieCislo = 5L;
+        float desatinneCislo = 5.5f;
+        double desatinneVacsieCislo = 5.5;
+        char znak = 'a';
+        String retazec = "retazec";
+        boolean bool = false;
+
+        int podiel = 2/3;
+        System.out.println(podiel);
+    }
 }
